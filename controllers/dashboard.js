ccbe37e9052b40776c61c5b7135727eb64fd1090
Tiffany Iong <tiffanyiong924@gmail.com>
2021-08-10 01:28:28 -0400

    done all the product-crud

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,6 +1,8 @@
 const Product = require('../models/product');
 const Users = require('../models/user');
 const categories = ['Bestseller', 'Popular','Other'];
+const { cloudinary } = require("../cloudinary");
+const product = require('../models/product');
 
 module.exports.index = async (req, res) =>{
     if(req.user.role != process.env.DASHBOARD_ACCESS){
@@ -33,11 +35,12 @@ module.exports.addNewProduct = async (req, res, next) => { //used catchAsync
         res.redirect('/login');
     }     
     if(!req.body.product) throw new ExpressError('Invalid Product Data', 400); //400 invalid client status code
-    
+      
         const product = new Product(req.body.product);
+        product.image = req.files.map(forimage => ({url:forimage.path, filename:forimage.filename}));
         await product.save();
         req.flash('success', 'Successfully add a product!')
-        console.log("attemped add data")
+        console.log(product);
         res.redirect(`/dashboard/product/${product._id}`)
    
 };
@@ -69,6 +72,17 @@ module.exports.editProduct = async (req, res) =>{
     const { id } = req.params;
     //split the object (we ground things like Product[name])
     const product = await Product.findByIdAndUpdate(id, {...req.body.product }, {new: true})
+    const imgs = req.files.map(forimage => ({url:forimage.path, filename:forimage.filename}));
+    product.image.push(...imgs);
+    await product.save();
+
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+       console.log(req.body.deleteImages)
+        await product.updateOne({ $pull: { image: { filename: { $in: req.body.deleteImages } } } });
+    }
     req.flash('success', 'Successfully made changes!')
     res.redirect(`/dashboard/product/${product._id}/`);
 
@@ -79,10 +93,22 @@ module.exports.deleteProduct = async (req, res) => {
         req.flash('error', 'Sorry, you do not have access to the page');
         res.redirect('/');
     } 
-    const { id } = req.params;
+    const { id } = req.params;  
+    const product = await Product.findById(req.params.id);
+    
+    var filenames = []
+    product.image.forEach((f) => {
+        filenames.push(f['filename'])
+    })
+    for(let i = 0; i < filenames.length; i++){
+        await cloudinary.uploader.destroy(filenames[i]);
+    }
+    
     await Product.findByIdAndDelete(id);
-    console.log("----delete----")
+    console.log("----------------delete----------------")
+   
     res.redirect('/dashboard/product');
+  
 };
 
 module.exports.renderUser = async (req, res) => {
--- routes/backend.js
+++ routes/backend.js
@@ -9,30 +9,29 @@ const { isLoggedIn } = require('../middleware');
 const multer = require('multer')
 const { storage } = require('../cloudinary');
 const upload = multer({ storage });
-//usefor
-//if you add more categories, please remember add this in this array
-const Users = require('../models/user');
-const categories = ['Bestseller', 'Popular','Other'];
+
+
 const dashboard = require('../controllers/dashboard');
 
 
 router.get('/', isLoggedIn, catchAsync (dashboard.index));
-
-router.get('/product',isLoggedIn, catchAsync (dashboard.renderProduct));
-
 router.get('/product/new', isLoggedIn, catchAsync(dashboard.renderNewProductForm));
 
-router.post('/product', isLoggedIn, catchAsync (dashboard.addNewProduct));
+router.route('/product')
+    .get(isLoggedIn, catchAsync (dashboard.renderProduct))
+    .post(isLoggedIn, upload.array('image'),catchAsync (dashboard.addNewProduct));
+   
 
-router.get('/product/:id', isLoggedIn, catchAsync (dashboard.showSingleProduct));
+router.route('/product/:id')
+    .get(isLoggedIn, catchAsync (dashboard.showSingleProduct))
+    .put(isLoggedIn, upload.array('image'),catchAsync (dashboard.editProduct))
+    .delete(isLoggedIn, catchAsync (dashboard.deleteProduct));
 
-router.get('/product/:id/edit', isLoggedIn, catchAsync (dashboard.renderEditProductForm));
 
-router.put('/product/:id', isLoggedIn, catchAsync (dashboard.editProduct));
 
-router.delete('/product/:id', isLoggedIn, catchAsync (dashboard.deleteProduct));
+router.get('/product/:id/edit', isLoggedIn, catchAsync(dashboard.renderEditProductForm));
 
-router.get('/user', isLoggedIn, catchAsync(dashboard.renderUser))
+router.get('/user', isLoggedIn, catchAsync(dashboard.renderUser));
 
 
 router.get('/logout', (req, res) => {
